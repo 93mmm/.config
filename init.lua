6e0f5d5481b452f09747b0aa8f143b00d8bc89c0
Alexander Reshetnyak <nickolaev513@gmail.com>
2023-12-10 10:06:38 +0300

    some commands

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+-- Automatic packer installation
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- configuring packer and installing plugins
 vim.cmd [[packadd packer.nvim]]
 
@@ -45,9 +59,13 @@ require("packer").startup(function(use)
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {
-      'nvim-tree/nvim-web-devicons', -- optional
+      'nvim-tree/nvim-web-devicons',
     },
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 
 end)
 
@@ -138,9 +156,9 @@ require("nvim-tree").setup({
   },
 })
 
-vim.keymap.set("n", "<leader>bt", "<cmd>NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>bf", "<cmd>NvimTreeFocus<CR>")
-vim.keymap.set("n", "<leader>bc", "<cmd>NvimTreeCollapse<CR>")
+vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>tf", "<cmd>NvimTreeFocus<CR>")
+vim.keymap.set("n", "<leader>tc", "<cmd>NvimTreeCollapse<CR>")
 
 
 -- NViM remaps
