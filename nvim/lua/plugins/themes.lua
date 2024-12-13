418dbe820e5850009a7d60378f19a7762cdb5144
93mmm <nickolaev513@gmail.com>
2024-12-13 15:02:53 +0300

    deleted preview

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,13 +1,13 @@
 return {
-    "scottmckendry/cyberdream.nvim",
+    "catppuccin/nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        require("cyberdream").setup({
+        require("catppuccin").setup({
             transparent = true,
             italic_comments = true,
             borderless_telescope = false,
         })
-        vim.cmd("colorscheme cyberdream")
+        vim.cmd("colorscheme catppuccin-mocha")
     end
 }
